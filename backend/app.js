05307dbfc8c47424004f9a9a9f6de0ea5c483357
Arthur <arthur.meunier@gmail.com>
2021-10-28 14:55:49 +0200

    user path ok ; start sauce routes

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const path = require('path');
+
+
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauce');
+
+
+
+mongoose.connect('mongodb+srv://arthurmeunier:MehCyMfL3yNOn8Pu@cluster0.5cgyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+
+module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// FONCTION SIGNUP //
+exports.signup = (req, res, next) => {
+  console.log('route Signup');
+  // On crypte le mot de passe
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      // On crée un nouvel user avec le mot de passe crypté et le mail de la requête
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+      // On enregistre l'user dans la BDD
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+// FONCTION LOGIN //
+exports.login = (req, res, next) => {
+  console.log('route Login');
+  User.findOne({ email: req.body.email })
+  // On récupère l'user de la base qui correspond à l'adresse mail entrée
+    .then(user => {
+      if (!user) {
+        // Si le mail n'est pas bon on envoie une erreur
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+      // On compare le mot de passe entré avec le hash stocké en BDD :
+      // --- Si la comparaison n'est pas bonne on renvoie une erreur
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          // --- Si la comparaison est bonne, on renvoie l'userId est un token
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
